ed00bbccbcf2384f93b7518295b02e0f32bf3ca7
Dyson <dysontilano@gmail.com>
2023-01-20 18:17:16 -0500

    Removed unused Repository method

--- backend/farmacias-backend-api-java/src/main/java/com/project/dev/api/repository/TipoPersonaRepository.java
+++ backend/farmacias-backend-api-java/src/main/java/com/project/dev/api/repository/TipoPersonaRepository.java
@@ -32,14 +32,6 @@ import org.springframework.data.repository.query.Param;
 public interface TipoPersonaRepository extends JpaRepository<TipoPersona, Long> {
 
     /**
-     * Obtiene los registros de la base de datos con un id indicado.
-     *
-     * @param id es el id a buscar en base de datos.
-     * @return los registros obtenidos.
-     */
-    public List<TipoPersona> findByIntIdTipoPersona(Long id);
-
-    /**
      * Busca en base de datos los registros que contengan una cadena específica.
      *
      * @param strSearch la cadena a buscar en base de datos.
