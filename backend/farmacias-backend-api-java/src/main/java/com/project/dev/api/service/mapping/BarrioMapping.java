ca477a318814210043062d35c75d20b68132bcc9
unknown <dysontilano@gmail.com>
2024-11-24 23:07:14 -0500

    Documentation updated and source code refactorized

--- backend/farmacias-backend-api-java/src/main/java/com/project/dev/api/service/mapping/BarrioMapping.java
+++ backend/farmacias-backend-api-java/src/main/java/com/project/dev/api/service/mapping/BarrioMapping.java
@@ -29,23 +29,21 @@ import org.mapstruct.Mapping;
 public interface BarrioMapping extends GenericMapping<BarrioDTO, Barrio> {
 
     /**
-     * Obtiene una entidad en base a su DTO.
+     * Obtiene una entidad en base a su DTO usando el campo clave de la base de datos.
      *
      * @param dto es el DTO a convertir en entidad.
      * @return la entidad equivalente al dto.
      */
-    // Deben ser el campo clave de la base de datos.
     @Mapping(source = "intIdBarrio", target = "intIdBarrio")
     @Override
     public Barrio getEntity(BarrioDTO dto);
 
     /**
-     * Obtiene un DTO en base a su entidad.
+     * Obtiene un DTO en base a su entidad usando el campo clave de la base de datos.
      *
      * @param entity es la entidad a convertir en DTO.
      * @return el dto equivalente a la entidad.
      */
-    // Deben ser el campo clave de la base de datos.
     @Mapping(source = "intIdBarrio", target = "intIdBarrio")
     @Override
     public BarrioDTO getDto(Barrio entity);
